8d8c4d6c4f803ccbc060baed49577993deb2bab3
formegusto <formegusto@gmail.com>
2021-06-03 14:38:58 +0900

    work screen

--- src/components/WorkComponent.tsx
+++ src/components/WorkComponent.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { DefaultHeadScreen, FullScreen } from "../styles/Screens";
+import styled from "styled-components";
+import {
+  DefaultContentScreen,
+  DefaultHeadScreen,
+  FullScreen,
+} from "../styles/Screens";
+import art from "../assets/art/art_4.png";
 
 function WorkComponent() {
   return (
@@ -17,8 +23,31 @@ function WorkComponent() {
       >
         <h1>WORK</h1>
       </DefaultHeadScreen>
+      <DefaultContentScreen>
+        <WorkBlock></WorkBlock>
+      </DefaultContentScreen>
+      <ArtBlock>
+        <img src={art} alt="Work Art" />
+      </ArtBlock>
     </FullScreen>
   );
 }
+
+const WorkBlock = styled.div`
+  height: 600px;
+`;
+
+const ArtBlock = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 400px;
+  height: 400px;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default WorkComponent;
